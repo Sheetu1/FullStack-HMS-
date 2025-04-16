d44113ab175e62862717ce700df09166d25143e7
Sheetu1 <sheetusharma548@gmail.com>
2025-04-16 23:40:54 +0530

    Message controller done

--- Backend/src/controllers/messageController.js
+++ Backend/src/controllers/messageController.js
@@ -1,4 +1,21 @@
-export const sendMessage = async(req,res,next) => {
-    const {firstName,lastName,email,phone,message} = req.body;
-
-}
+import messageModel from "../models/messageSchema.js";
+export const sendMessage = async (req, res, next) => {
+  const { firstName, lastName, email, phone, message } = req.body;
+  if (!firstName || !lastName || !email || !phone || !message) {
+    return res.status(400).json({
+      success: false,
+      message: "All filelds are required",
+    });
+    await messageModel.create({
+      firstName,
+      lastName,
+      email,
+      phone,
+      message,
+    });
+  }
+  res.status(200).json({
+    success: true,
+    message: "Message send successfully",
+  });
+};
