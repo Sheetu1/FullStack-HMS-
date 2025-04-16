dc138b1ec7c8e0ae1bcce550c4a1d1132b19b1a7
Sheetu1 <sheetusharma548@gmail.com>
2025-04-16 23:12:14 +0530

    Messsage Schema done

---
+++ Backend/src/models/messageSchema.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import validator from 'express-validator'
+
+const messageSchema = new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+        minLength:[3, "First Name must  contain at least 3 characters"]
+    },
+    lastName:{
+        type:String,
+        required:true,
+        minLength:[3, "Last Name must  contain at least 3 characters"]
+    },
+    email:{
+        type:String,
+        required:true,
+        validate: [validator.isEmail,"Please provide a valid email"]
+    },
+    phone:{
+        type:String,
+        required:true,
+        minLength:[11, "Phone number must contain Excat 11 digits"],
+        maxLength:[11, "Phone number must contain Excat 11 digits"],
+    },
+    message:{
+        type:String,
+        required:true,
+        minLength:[10, "Message must contain at least 10 characters"],
+    },
+
+})
+
+export default Message = mongoose.model('Message',messageSchema);
